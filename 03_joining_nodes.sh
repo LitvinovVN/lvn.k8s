9382889b1f1a3637752add8ed45d4162977870db
LitvinovVN <LitvinovVN@rambler.ru>
2020-03-30 16:03:27 +0300

    f4

---
+++ 03_joining_nodes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+echo "---Joining nodes to Kubernetes cluster---"
+echo "Getting --token"
+kubeadm token list
+echo "---------------"
+echo "Getting --discovery-token-ca-cert-hash"
+openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | \
+   openssl dgst -sha256 -hex | sed 's/^.* //'
+echo "---------------"
