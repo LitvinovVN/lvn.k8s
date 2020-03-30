b39c754f7dcc0280339aaa46005cfd1e6e79bd41
LitvinovVN <LitvinovVN@rambler.ru>
2020-03-30 14:41:31 +0300

    k8s master node 1

---
+++ 02_config_k8s.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+echo "---Configuring Kubernetes cluster---"
+echo "Starting init master-node"
+kubeadm init --pod-network-cidr=192.168.0.0/16
+
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+watch kubectl get pods --all-namespaces
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+kubectl get nodes -o wide
+echo "OK"
+echo "---------------"
