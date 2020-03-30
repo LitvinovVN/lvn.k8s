cb89f07e53b576f6bcfdb21dbf7c6b7ffcc6bbe3
LitvinovVN <LitvinovVN@rambler.ru>
2020-03-30 13:13:46 +0300

    upd1

--- 01_install_pre.sh
+++ 01_install_pre.sh
@@ -10,6 +10,11 @@ apt-get install -y mc
 echo "OK"
 echo "---------------"
 
+echo "Installing CURL"
+apt-get install -y apt-transport-https curl
+echo "OK"
+echo "---------------"
+
 echo "Installing Docker..."
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get install -y \
@@ -18,7 +23,7 @@ apt-get install -y \
     curl \
     gnupg-agent \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -31,3 +36,12 @@ echo "Docker hello-world test"
 docker run hello-world
 echo "OK"
 echo "---------------"
+
+echo "Installing kubeadm, kubelet and kubectl"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
