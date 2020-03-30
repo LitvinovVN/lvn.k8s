d93d1cb73d2391b037d45b42da750e6ce70ef599
LitvinovVN <LitvinovVN@rambler.ru>
2020-03-30 12:34:04 +0300

    docker

--- 01_install_pre.sh
+++ 01_install_pre.sh
@@ -9,3 +9,24 @@ echo "Installing Midnight Commander..."
 apt-get install -y mc
 echo "OK"
 echo "---------------"
+
+echo "Installing Docker..."
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+echo "Docker hello-world test"
+docker run hello-world
+echo "OK"
+echo "---------------"
