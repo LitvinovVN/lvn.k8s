49f62cb5882095b4baa8267f650d8fc1db647051
LitvinovVN <LitvinovVN@rambler.ru>
2020-03-30 16:09:59 +0300

    f5

--- 03_joining_nodes.sh
+++ 03_joining_nodes.sh
@@ -3,6 +3,7 @@ clear
 echo "---Joining nodes to Kubernetes cluster---"
 echo "Getting --token"
 kubeadm token list
+echo "If token has expired: kubeadm token create"
 echo "---------------"
 echo "Getting --discovery-token-ca-cert-hash"
 openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | \
