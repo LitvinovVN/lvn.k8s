0602daf0e2c05b840c07b9b864192c4ec380774b
LitvinovVN <LitvinovVN@rambler.ru>
2020-03-31 15:24:17 +0300

    31032006

--- 04_install_helm.sh
+++ 04_install_helm.sh
@@ -4,6 +4,7 @@ echo "Installing Helm..."
 wget https://get.helm.sh/helm-v3.1.2-linux-amd64.tar.gz
 tar xzvf helm-v3.1.2-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
+rm helm-v3.1.2-linux-amd64.tar.gz
 echo "OK"
 
 echo "Checking Helm version..."
