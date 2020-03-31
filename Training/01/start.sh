4671bb4c66398975b9ab01f6d53e095581a2e2a4
LitvinovVN <LitvinovVN@rambler.ru>
2020-03-31 17:47:49 +0300

    training 01-2

--- Training/01/start.sh
+++ Training/01/start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 clear
 kubectl create -f pod-nginx.yaml
+echo "kubectl get pod -o wide"
 kubectl get pod -o wide
+
+echo "For editing pod-nginx enter:"
+echo "kubectl edit pod pod-nginx"
+echo ""
+echo "For deliting pod-nginx enter:"
+echo "kubectl delete pod pod-nginx"
