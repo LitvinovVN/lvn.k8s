d20ac0bccaef89171bebd3a45930486860a8c25f
LitvinovVN <LitvinovVN@rambler.ru>
2020-04-01 10:49:30 +0300

    hello-app

---
+++ Training/03 Ingress/hello-app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+echo "kubectl create namespace dev"
+kubectl create namespace dev
+echo ""
+
+echo "kubectl create -f hello-app.yaml"
+kubectl create -f hello-app.yaml
+echo ""
+
+echo "kubectl get deployments -n dev"
+kubectl get deployments -n dev
+echo ""
+
+echo "kubectl create -f hello-app-service.yaml"
+kubectl create -f hello-app-service.yaml
+echo ""
+
+echo "kubectl get svc -n dev"
+kubectl get svc -n dev
+echo ""
